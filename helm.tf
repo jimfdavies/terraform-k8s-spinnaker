b31e701902c239b3340a7e31e4d2f94ee8dfb06e
Jim Davies <jimfdavies@googlemail.com>
2018-10-18 00:32:39 +0100

    Comment out non-working helm config

--- helm.tf
+++ helm.tf
@@ -1,16 +1,16 @@
-provider "helm" {
-  kubernetes {
-      host     = "${google_container_cluster.spinnaker1.endpoint}"
-      username = "${var.master_username}"
-      password = "${var.master_password}"
-
-      client_certificate     = "${google_container_cluster.spinnaker1.master_auth.0.client_certificate}"
-      client_key             = "${google_container_cluster.spinnaker1.master_auth.0.client_key}"
-      cluster_ca_certificate = "${google_container_cluster.spinnaker1.master_auth.0.cluster_ca_certificate}"
-  }
-}
-
-resource "helm_release" "spinnaker" {
-  name = "spinnaker"
-  chart = "stable/spinnaker"
-}
+# provider "helm" {
+#   kubernetes {
+#       host     = "${google_container_cluster.spinnaker1.endpoint}"
+#       username = "${var.master_username}"
+#       password = "${var.master_password}"
+#
+#       client_certificate     = "${google_container_cluster.spinnaker1.master_auth.0.client_certificate}"
+#       client_key             = "${google_container_cluster.spinnaker1.master_auth.0.client_key}"
+#       cluster_ca_certificate = "${google_container_cluster.spinnaker1.master_auth.0.cluster_ca_certificate}"
+#   }
+# }
+#
+# resource "helm_release" "spinnaker" {
+#   name = "spinnaker"
+#   chart = "stable/spinnaker"
+# }
